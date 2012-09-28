804001946a5643bb9cd969ff5f6f965ddaefed58
Łukasz Nidecki <l.nidecki@cheesecat.com>
2012-09-28 13:27:27 +0200

    test db access

--- protected/config/main.php
+++ protected/config/main.php
@@ -50,10 +50,10 @@ return array(
 		// uncomment the following to use a MySQL database
 
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=fbhack',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'fbhack',
+			'password' => 'fbhack',
 			'charset' => 'utf8',
 		),
 
