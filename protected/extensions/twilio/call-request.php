47b27fb0d2d8832207f7f387c57f5fbbac0a9b54
VILQ <vilq@naprezydenta.com>
2012-09-28 15:25:11 +0200

    name/loc fix

--- protected/extensions/twilio/call-request.php
+++ protected/extensions/twilio/call-request.php
@@ -15,11 +15,17 @@
 	// Instantiate a new Twilio Rest Client
 	$client = new Services_Twilio($sid, $token, $version);
 
+        
+        //get phone numbers from api
 //        $friendlist = array('48509988585','48605965576','48889708906');
         $friendlist = array('48509988585');
 
-        $name = 'Simon';
-        $location = 'Facebook Hack for developers';
+        
+        //get name from fb
+        $name = array('name' => 'Simon', 'surname' => 'Says');
+        
+        //get location by gps coords
+        $location = array('name' => 'Facebook Hack for developers', 'street' => 'Marii Konopnickiej 6', 'city' => 'Warsaw'); // places_get_by_coords
         
         foreach ($friendlist as $friend)
 	try {
@@ -27,7 +33,7 @@
 		$call = $client->account->calls->create(
 			$phonenumber, // The number of the phone initiating the call
     			$friend, // The number of the phone receiving call
-			'http://vilq.net/twilio/message.php?name='.urlencode($name).'&location='.urlencode($location) // The URL Twilio will request when the call is answered
+			'http://vilq.net/twilio/message.php?name='.urlencode(implode(" ",$name)).'&location='.urlencode(implode(" ",$location)) // The URL Twilio will request when the call is answered
 		);
 		echo 'Started call: ' . $call->sid."<br/>";
 	} catch (Exception $e) {
