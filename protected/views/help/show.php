0ac945a29b8ba56f9e8869d0db4540ac89e700f9
Łukasz Nidecki <l.nidecki@cheesecat.com>
2012-09-28 15:47:48 +0200

    nie chce mi sie bawic z urlem

--- protected/views/help/show.php
+++ protected/views/help/show.php
@@ -13,7 +13,7 @@
     <meta property="og:description" content="Potrzebuje pomocy" />
     <meta property="fb:app_id" content="161728350617606" />
     <meta property="og:url" content="<?php echo($this->createAbsoluteUrl('help/show', array('id'=>$model->id))); ?>" />
-    <meta property="og:image" content="<?php echo $model->image ? $this->createAbsoluteUrl('/images/' . $model->id . '_' . $model->image) : $this->createAbsoluteUrl('/images/payu.jpg');?>" />
+    <meta property="og:image" content="<?php echo $model->image ? ('http://hack.ccat.pl/images/' . $model->id . '_' . $model->image) : $this->createAbsoluteUrl('/images/payu.jpg');?>" />
     <meta property="drunk-help:location:latitude"  content="<?php echo $model->lat; ?>">
     <meta property="drunk-help:location:longitude" content="<?php echo $model->long; ?>">
     <title></title>
