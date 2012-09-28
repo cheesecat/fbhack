13bb5bd04bc577179764c4a54c07b88c728213aa
VILQ <vilq@naprezydenta.com>
2012-09-28 17:40:55 +0200

    fb->api

--- protected/extensions/places/places.php
+++ protected/extensions/places/places.php
@@ -1,41 +1,28 @@
 <?php
 
-    function get($url){
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate'); 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($ch, CURLOPT_ENCODING, "UTF-8" );  
-        $output = curl_exec($ch);
-        curl_close ($ch);     
-        return json_decode($output);
+    $lat = '52.228391';
+    $lon = '21.025815';
+    $token = 'AAACEdEose0cBANHdoTZBnwxuTcvlbpuGDCdUK1bVdRNsf67PKuTbnyQ5JFkEWTR8mscfZAIvn9DEfZCW5CScMRVlo0ROUjGNSo2kMLN75KT4OjeZAeMg';
+
+    function get_location($lat, $lon, $token)
+    {
+        $uri = '/search?type=place&center=' . $lat . ',' . $lon . '&distance=100&access_token=' . $token;
+
+        $fb = new Facebook();
+
+        $output = $fb->api($uri);
+        $loc = array();
+        $loc['street'] = '';
+        $i = 0;
+        while ($loc['street']==''){
+            $loc['name'] = $output->data[$i]->name;
+            $loc['street'] = str_replace("ul. ", "", $output->data[$i]->location->street);
+            $loc['city'] = $output->data[$i]->location->city;
+            $i++;
+            if ($i >= count($output->data)) exit;
+        }
+        return $loc;
     }
-
-	$lat = '52.228391';
-	$lon = '21.025815';
-	$token = 'AAACEdEose0cBANHdoTZBnwxuTcvlbpuGDCdUK1bVdRNsf67PKuTbnyQ5JFkEWTR8mscfZAIvn9DEfZCW5CScMRVlo0ROUjGNSo2kMLN75KT4OjeZAeMg';
-
-function get_location($lat, $lon, $token)
-{
-    $base = 'https://graph.facebook.com';
-    $uri = '/search?type=place&center=' . $lat . ',' . $lon . '&distance=100&access_token=' . $token;
-
-    $output = get($base . $uri);
-    $loc = array();
-    $loc['street'] = '';
-    $i = 0;
-    while ($loc['street']==''){
-        $loc['name'] = $output->data[$i]->name;
-        $loc['street'] = str_replace("ul. ", "", $output->data[$i]->location->street);
-        $loc['city'] = $output->data[$i]->location->city;
-        $i++;
-        if ($i >= count($output->data)) exit;
-    }
-    return $loc;
-}
 
 //$loc = get_location($lat, $lon, $token);
 	
