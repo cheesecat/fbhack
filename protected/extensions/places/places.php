dfc5b17a2285fee795c7b83570b6bab941dc04e4
VILQ <vilq@naprezydenta.com>
2012-09-28 15:21:17 +0200

    places + twilio

---
+++ protected/extensions/places/places.php
@@ -0,0 +1,30 @@
+<?php
+
+    function get($url){
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate'); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_ENCODING, "UTF-8" );  
+        $output = curl_exec($ch);
+        curl_close ($ch);     
+        return json_decode($output);
+    }
+
+	$lat = '52.228391';
+	$lon = '21.025815';
+	$token = 'AAAAAAITEghMBALZA7rzKtrYKAJMJpSVMEn6s616Q653kFaTNTKxZBFLWsqfmbgaHHggmnZCvnYaIsktG7zeFzbrgDStKLtidS969eAuV2Niz4cAXjJU';
+	
+	$uri = 'https://graph.facebook.com/search?type=place&center='.$lat.','.$lon.'&distance=100&access_token='.$token;
+
+	$output = get($uri);
+	$loc = array();
+	$loc['name'] = $output->data[0]->name;
+	$loc['street'] = str_replace("ul. ","",$output->data[0]->location->street);
+	$loc['city'] = $output->data[0]->location->city;
+	
+	print_r($loc);
+?>	
